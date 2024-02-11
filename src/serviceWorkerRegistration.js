e7a5ee8f7c4859f60c201e72e5955020e155b633
deepanshu-buku <deepanshu.somani@bukuwarung.com>
2024-02-11 16:39:22 +0530

    removed public_url

--- src/serviceWorkerRegistration.js
+++ src/serviceWorkerRegistration.js
@@ -30,7 +30,7 @@ export function register(config) {
         }
 
         window.addEventListener('load', () => {
-            const swUrl = `/service-worker.js`;
+            const swUrl = `https://deepanshu-somani.vercel.app/service-worker.js`;
 
             if (isLocalhost) {
                 // This is running on localhost. Let's check if a service worker still exists or not.
